6bee96892e5cd2f1202d6f88473753fbf0aeaee5
Evan <evanzhoudev@gmail.com>
2023-12-01 10:42:49 +0000

    Replace Makefile with build.sh

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Birtual Machine for [Elisp](https://github.com/tsoding/elisp)
 # Quick Start
 
 ```console
-$ make -B
+$ ./build.sh    # or ./build_asyc.bat if you are on Windows otherwise also you can use `make -B` on Linux if cc path has wrong, please use it or you can change cc path as gcc like CC=gcc to use ./build.sh
 $ make examples
 $ ./bme -i ./examples/fib.bm
 $ ./bme -i ./examples/e.bm
---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -xe
+
+CC=${CC:=/nix/store/dq0xwmsk1g0i2ayg6pb7y87na2knzylh-gcc-wrapper-11.3.0/bin/cc}
+CFLAGS="-Wall -Wextra -Wswitch-enum -Wmissing-prototypes -O3  -pedantic -std=c11"
+LIBS=
+
+$CC $CFLAGS -o basm ./src/basm.c $LIBS
+$CC $CFLAGS -o bme ./src/bme.c $LIBS
+$CC $CFLAGS -o debasm ./src/debasm.c $LIBS
+
+for example in `find examples/ -name \*.basm | sed "s/\.basm//"`; do
+  ./basm "$example.basm" "$example.bm"
+done
